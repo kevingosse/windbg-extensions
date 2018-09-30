fa2126fb5424feccc1d4261f78084c15abba9a0e
Kevin Gosse <krix33@gmail.com>
2018-09-30 21:49:05 +0200

    Cleanup in C# runner

--- src/WinDbgScriptRunner.Shared/Runner.cs
+++ src/WinDbgScriptRunner.Shared/Runner.cs
@@ -1,106 +1,26 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using Microsoft.Diagnostics.Runtime;
-using Microsoft.Diagnostics.Runtime.Interop;
 using RGiesecke.DllExport;
 
 namespace WindbgScriptRunner
 {
     public class Runner
     {
-        public static IDebugClient DebugClient { get; private set; }
-        public static DataTarget DataTarget { get; private set; }
-        public static ClrRuntime Runtime { get; private set; }
-
-        public static AppDomain _childDomain;
+        public static AppDomain ChildDomain;
 
         [DllExport("compileandrun")]
-        public static void Script(IntPtr client, [MarshalAs(UnmanagedType.LPStr)] string args)
+        public static void CompileAndRun(IntPtr client, [MarshalAs(UnmanagedType.LPStr)] string args)
         {
-            if (_childDomain == null)
+            if (ChildDomain == null)
             {
-                _childDomain = AppDomain.CreateDomain("CSharpRunner", AppDomain.CurrentDomain.Evidence, GetCurrentAssemblyPath(), ".", false);
+                ChildDomain = AppDomain.CreateDomain("CSharpRunner", AppDomain.CurrentDomain.Evidence, GetAssemblyPath(), ".", false);
             }
 
             var invoker = new ScriptInvoker(client, args);
 
-            _childDomain.DoCallBack(invoker.Invoke);
-        }
-
-        public static bool InitApi(IntPtr ptrClient)
-        {
-            // On our first call to the API:
-            //   1. Store a copy of IDebugClient in DebugClient.
-            //   2. Replace Console's output stream to be the debugger window.
-            //   3. Create an instance of DataTarget using the IDebugClient.
-            if (DebugClient == null)
-            {
-                object client = Marshal.GetUniqueObjectForIUnknown(ptrClient);
-                DebugClient = (IDebugClient)client;
-
-                var stream = new StreamWriter(new DebugEngineStream(DebugClient));
-                stream.AutoFlush = true;
-                Console.SetOut(stream);
-
-                DataTarget = DataTarget.CreateFromDebuggerInterface(DebugClient);
-            }
-
-            // If our ClrRuntime instance is null, it means that this is our first call, or
-            // that the dac wasn't loaded on any previous call.  Find the dac loaded in the
-            // process (the user must use .cordll), then construct our runtime from it.
-            if (Runtime == null)
-            {
-                // Just find a module named mscordacwks and assume it's the one the user
-                // loaded into windbg.
-                using (var process = Process.GetCurrentProcess())
-                {
-                    foreach (ProcessModule module in process.Modules)
-                    {
-                        if (module.FileName.ToLower().Contains("mscordacwks"))
-                        {
-                            // TODO:  This does not support side-by-side CLRs.
-                            Runtime = DataTarget.ClrVersions.Single().CreateRuntime(module.FileName);
-                            break;
-                        }
-                    }
-                }
-
-                // Otherwise, the user didn't run .cordll.
-                if (Runtime == null)
-                {
-                    Console.WriteLine("Mscordacwks.dll not loaded into the debugger.");
-                    Console.WriteLine("Run .cordll to load the dac before running this command.");
-                }
-            }
-            else
-            {
-                // If we already had a runtime, flush it for this use.  This is ONLY required
-                // for a live process or iDNA trace.  If you use the IDebug* apis to detect
-                // that we are debugging a crash dump you may skip this call for better perf.
-                Runtime.Flush();
-
-                // Temporary workaround for a bug in ClrMD
-                // To be removed when https://github.com/Microsoft/clrmd/pull/94 get published on NuGet
-                var type = Type.GetType("Microsoft.Diagnostics.Runtime.Desktop.DesktopRuntimeBase, Microsoft.Diagnostics.Runtime");
-
-                var modules = type.GetField("_modules", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Runtime)
-                    as IDictionary;
-
-                modules?.Clear();
-
-                var moduleFiles = type.GetField("_moduleFiles", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Runtime)
-                    as IDictionary;
-
-                moduleFiles?.Clear();
-            }
-
-            return Runtime != null;
+            ChildDomain.DoCallBack(invoker.Invoke);
         }
 
         [DllExport("DebugExtensionInitialize")]
@@ -113,12 +33,9 @@ namespace WindbgScriptRunner
             return 0;
         }
 
-        static uint DEBUG_EXTENSION_VERSION(uint Major, uint Minor)
-        {
-            return ((((Major) & 0xffff) << 16) | ((Minor) & 0xffff));
-        }
+        private static uint DEBUG_EXTENSION_VERSION(uint Major, uint Minor) => ((Major & 0xffff) << 16) | (Minor & 0xffff);
 
-        private static string GetCurrentAssemblyPath()
+        private static string GetAssemblyPath()
         {
             string basePath;
             
--- src/WinDbgScriptRunner.Shared/ScriptRunner.cs
+++ src/WinDbgScriptRunner.Shared/ScriptRunner.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.CodeDom.Compiler;
+using System.Collections;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using Microsoft.CSharp;
+using Microsoft.Diagnostics.Runtime;
+using Microsoft.Diagnostics.Runtime.Interop;
 
 namespace WindbgScriptRunner
 {
     public class ScriptRunner
     {
+        public static IDebugClient DebugClient { get; private set; }
+        public static DataTarget DataTarget { get; private set; }
+        public static ClrRuntime Runtime { get; private set; }
+
         public static void Run(IntPtr client, [MarshalAs(UnmanagedType.LPStr)] string args)
         {
-            if (!Runner.InitApi(client))
+            if (!InitApi(client))
             {
                 return;
             }
@@ -48,13 +56,83 @@ namespace WindbgScriptRunner
 
                     var method = type.GetMethod("Run", BindingFlags.Static | BindingFlags.Public);
 
-                    method.Invoke(null, new object[] { Runner.Runtime.Heap });
+                    method.Invoke(null, new object[] { Runtime.Heap });
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("An error occured: " + ex);
             }
+        }
+
+        private static bool InitApi(IntPtr ptrClient)
+        {
+            // On our first call to the API:
+            //   1. Store a copy of IDebugClient in DebugClient.
+            //   2. Replace Console's output stream to be the debugger window.
+            //   3. Create an instance of DataTarget using the IDebugClient.
+            if (DebugClient == null)
+            {
+                object client = Marshal.GetUniqueObjectForIUnknown(ptrClient);
+                DebugClient = (IDebugClient)client;
+
+                var stream = new StreamWriter(new DebugEngineStream(DebugClient));
+                stream.AutoFlush = true;
+                Console.SetOut(stream);
+
+                DataTarget = DataTarget.CreateFromDebuggerInterface(DebugClient);
+            }
+
+            // If our ClrRuntime instance is null, it means that this is our first call, or
+            // that the dac wasn't loaded on any previous call.  Find the dac loaded in the
+            // process (the user must use .cordll), then construct our runtime from it.
+            if (Runtime == null)
+            {
+                // Just find a module named mscordacwks and assume it's the one the user
+                // loaded into windbg.
+                using (var process = Process.GetCurrentProcess())
+                {
+                    foreach (ProcessModule module in process.Modules)
+                    {
+                        if (module.FileName.ToLower().Contains("mscordacwks"))
+                        {
+                            // TODO:  This does not support side-by-side CLRs.
+                            Runtime = DataTarget.ClrVersions.Single().CreateRuntime(module.FileName);
+                            break;
+                        }
+                    }
+                }
+
+                // Otherwise, the user didn't run .cordll.
+                if (Runtime == null)
+                {
+                    Console.WriteLine("Mscordacwks.dll not loaded into the debugger.");
+                    Console.WriteLine("Run .cordll to load the dac before running this command.");
+                }
+            }
+            else
+            {
+                // If we already had a runtime, flush it for this use.  This is ONLY required
+                // for a live process or iDNA trace.  If you use the IDebug* apis to detect
+                // that we are debugging a crash dump you may skip this call for better perf.
+                Runtime.Flush();
+
+                // Temporary workaround for a bug in ClrMD
+                // To be removed when https://github.com/Microsoft/clrmd/pull/94 get published on NuGet
+                var type = Type.GetType("Microsoft.Diagnostics.Runtime.Desktop.DesktopRuntimeBase, Microsoft.Diagnostics.Runtime");
+
+                var modules = type.GetField("_modules", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Runtime)
+                    as IDictionary;
+
+                modules?.Clear();
+
+                var moduleFiles = type.GetField("_moduleFiles", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Runtime)
+                    as IDictionary;
+
+                moduleFiles?.Clear();
+            }
+
+            return Runtime != null;
         }
     }
 }
