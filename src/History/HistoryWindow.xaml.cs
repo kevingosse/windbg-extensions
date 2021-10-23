de90f89b718d479977a74b2608b59144519a8e96
Kevin Gosse <krix33@gmail.com>
2021-10-23 17:40:12 +0200

    Update history extension for the latest windbg versions

--- src/History/HistoryWindow.xaml.cs
+++ src/History/HistoryWindow.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace WinDbgExt.History
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WinDbgExt.History
 {
     public partial class HistoryWindow
     {
         public HistoryWindow()
         {
             InitializeComponent();
+        }
+
+        private void HistoryList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var listBox = (ListBox)sender;
+
+            var viewModel = (HistoryWindowViewModel)listBox.DataContext;
+
+            var selectedItem = (Tuple<string, string>)listBox.SelectedItem;
+
+            viewModel.OpenCommand.Execute(selectedItem.Item2);
         }
     }
 }
